8a071fc08305a0842d6b7856283c56ca42e2cc85
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-02 10:13:25 +0100

    nav css & transform nav to include file

---
+++ include/nav.php
@@ -0,0 +1,6 @@
+<div class="nav">
+    <a href="index.php"><img class="Logo" src="img/Banner.png" alt="Logo"></a>
+    <a href="login.php"><p>login</p></a>
+    <a href="register.php"><p>Register</p></a>
+    <a href=""><p>Marketplace</p></a>
+  </div>
