2b9f9c8f3f61619ed53cbee62b173947062da3a3
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-02 21:11:00 +0100

    and logged cundition

--- include/nav.php
+++ include/nav.php
@@ -1,6 +1,22 @@
 <div class="nav">
     <a href="index.php"><img class="Logo" src="img/Banner.png" alt="Logo"></a>
+
+<?php 
+
+  if ($_SESSION['logged'] == 1)
+  {
+
+?>
+  <a href="disconnect.php"><p>Disconnect</p></a>
+  <a href="account.php"><p>My Account</p></a>
+<?php
+  }
+  else{
+?>
     <a href="login.php"><p>login</p></a>
     <a href="register.php"><p>Register</p></a>
-    <a href=""><p>Marketplace</p></a>
-  </div>
+<?php
+    }
+?>
+  <a href=""><p>Marketplace</p></a>
+</div>
