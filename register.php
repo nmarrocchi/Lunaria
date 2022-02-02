ba8fb67ffc4ab462171abba27910115a9b787337
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-02 11:29:53 +0100

    register script

--- register.php
+++ register.php
@@ -1,3 +1,25 @@
+<?php
+    session_start();
+    require("session.php");
+
+    if (isset($_POST["submit"])) {
+        $UserExist = $bdd->query("SELECT COUNT(*) FROM users WHERE username ='".$_POST['username']."'");
+        $UserExist = $UserExist->fetch();
+
+        if ($UserExist["COUNT(*)"] > 0) {
+            $CreateAccount = "This username is taken...";
+        } 
+        else {
+            $date = date('m/d/Y h:i:s a', time());
+            $bdd->query("INSERT INTO users(username, password, registration, isAdmin) VALUES('".$_POST['username']."','".$_POST['password']."', "$date",0)");
+            $CreateAccount = "Your account has been created";
+            
+        }
+    }
+    else{
+        $CreateAccount = "This username is taken...";
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,13 +38,15 @@
     <div class="Form">
         <form action="" method="post">
 
+            <p class="createMsg"><?php echo $CreateAccount?></p>
+
             <label for="username">Username : </label>
             <input type="text" name="username" required></input>
 
             <label for="password">Password :</label>
             <input type="text" name="password" required></input>
 
-            <input type="submit" value="register">
+            <input type="submit" name="submit" value="register">
 
         </form>
     </div>
