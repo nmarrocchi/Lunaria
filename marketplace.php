8178c1c9aab38b2212bc39c56f5d263098bf2ead
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-03 15:58:51 +0100

    Marketplace base page

---
+++ marketplace.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+    require("session.php");
+    CheckIfIsLog();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="style/nav.css">
+    <link rel="stylesheet" href="style/forms.css">
+    <title>Lunaria | Marketplace</title>
+</head>
+<body>
+
+    <?php include "include/nav.php" ?>
+
+    <div class="content">
+    </div>
+    
+</body>
+</html>
