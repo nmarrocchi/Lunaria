b0250633edd1cca4e97baa1da2c61c541bb93b41
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-03 15:59:07 +0100

    Add CheckIfIsLog function

--- session.php
+++ session.php
@@ -3,7 +3,7 @@
 
     try
     {
-        $bdd = new PDO('mysql:host=mysql-lunaria.alwaysdata.net; dbname=lunaria_lunaria', 'lunaria', '0ver_Draw070902');  
+        $bdd = new PDO('mysql:host=mysql-lunaria.alwaysdata.net; dbname=lunaria_lunaria', 'lunaria', '0ver_Draw070902');
     }
 
     catch(Exception $e)
@@ -12,10 +12,19 @@
     }
 
 
-/* Check if user is logged and if isn't, he can login */
+/* Check if user is logged and if isn't, return to index */
 function CheckIfCanBeLog(){
     if(isset($_SESSION['Logged'])){
         if($_SESSION['Logged'] == 1){
+            header('Location: index.php');
+        }
+    }
+}
+
+/* Check if user is logged and if is log, return to index */
+function CheckIfIsLog(){
+    if(isset($_SESSION['Logged'])){
+        if($_SESSION['Logged'] == 0){
             header('Location: index.php');
         }
     }
