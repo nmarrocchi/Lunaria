5d3c6773c57da4ce8c748e60b77156efd04463dd
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-05 22:12:02 +0100

    Add order list

--- account.php
+++ account.php
@@ -27,6 +27,30 @@
       <img class="guest" src="img/guest.png" alt="icon">
       <p>Welcome to Lunaria <?php echo $_SESSION['user'] ?></p>
       <p>You join Lunaria the <?php echo $date[0] ?></p>
+
+      <?php 
+        $orders = $bdd->query("SELECT * FROM orders WHERE user = '".$_SESSION["user"]."' ");
+        $totalOrder = 0;
+      ?>
+
+      <table class="userOrder">
+
+        <tr>
+          <td colspan="2" class="orderHeader">Orders</td>
+        </tr>
+        <?php while($ordersFishs = $orders->fetch()){ $totalOrder += $ordersFishs["price"]?>
+
+        <tr>
+          <td><?php echo $ordersFishs["fish_fr"] ?> / <?php echo $ordersFishs["fish_eng"] ?></td>
+          <td><?php echo $ordersFishs["price"] ?></td>
+        </tr>
+
+        <?php } ?>
+        <tr>
+          <td colspan="2" class="totalOrder"><p>Total order : <?php echo $totalOrder ?> flux</p></td>
+        </tr>
+      </table>
+
     </div>
     
 </body>
