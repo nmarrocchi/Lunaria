143ad6c2ab8c55e9178a0788c0f17a7109c99f7f
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-05 23:19:32 +0100

    nav responsive

--- include/nav.php
+++ include/nav.php
@@ -1,22 +1,26 @@
-<div class="nav">
-    <a href="index.php"><img class="Logo" src="img/Banner.png" alt="Logo"></a>
+<div class="banner">
+  <a href="index.php"><img class="Logo" src="img/Banner.png" alt="Logo"></a>
 
-<?php 
+  <div class="nav">
+  <?php 
 
-  if ($_SESSION['Logged'] == 1)
-  {
+    if ($_SESSION['Logged'] == 1)
+    {
 
-?>
-  <a href="disconnect.php"><p>Disconnect</p></a>
-  <a href="account.php"><p>My Account</p></a>
-  <a href="marketplace.php"><p>Marketplace</p></a>
-<?php
-  }
-  else{
-?>
-    <a href="login.php"><p>login</p></a>
-    <a href="register.php"><p>Register</p></a>
-<?php
+  ?>
+
+    <a href="marketplace.php"><p>Marketplace</p></a>
+    <a href="account.php"><p>My Account</p></a>
+    <a href="disconnect.php"><p>Disconnect</p></a>
+    
+  <?php
     }
-?>
+    else{
+  ?>
+      <a href="login.php"><p>login</p></a>
+      <a href="register.php"><p>Register</p></a>
+  <?php
+      }
+  ?>
+  </div>
 </div>
