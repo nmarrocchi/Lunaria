c0ac9f7e8a19f6aeed68be291ca3a92c86190f66
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-06 23:50:42 +0100

    remove liquid & rarity

--- marketplace.php
+++ marketplace.php
@@ -1,26 +1,3 @@
-<script>
-    // - Init all display
-    var elems = document.getElementsByClassName('waterfishs');
-    for (var i=0;i<elems.length;i+=1){
-        elems[i].style.display = 'none';
-    }
-
-    var elems = document.getElementsByClassName('chocofishs');
-    for (var i=0;i<elems.length;i+=1){
-        elems[i].style.display = 'none';
-    }
-
-    var elems = document.getElementsByClassName('lavafishs');
-    for (var i=0;i<elems.length;i+=1){
-        elems[i].style.display = 'none';
-    }
-
-    var elems = document.getElementsByClassName('plasmafishs');
-    for (var i=0;i<elems.length;i+=1){
-        elems[i].style.display = 'none';
-    }
-</script>
-
 <?php
     session_start();
     require("session.php");
@@ -83,8 +60,6 @@
                             </td>                                                                                                                                                                             
                             <td><img src="img/fish/water/<?php echo $fishlist['id'] ?>.png" alt="fish"></td>
                             <td><?php echo $fishlist['name_fr'] ?> / <?php echo $fishlist['name_eng'] ?></td>
-                            <td><?php echo $fishlist['liquid'] ?></td>
-                            <td><?php echo $fishlist['rarity'] ?></td>
                             <td><?php echo $fishlist['price'] ?></td>
                         </tr>                                                                                                                                                                                 
                                                                                                                                                                                                               
@@ -111,8 +86,6 @@
                             </td>                                                                                                                                                                             
                             <td><img src="img/fish/choco/<?php echo $fishlist['id'] ?>.png" alt="fish"></td>
                             <td><?php echo $fishlist['name_fr'] ?> / <?php echo $fishlist['name_eng'] ?></td>
-                            <td><?php echo $fishlist['liquid'] ?></td>
-                            <td><?php echo $fishlist['rarity'] ?></td>
                             <td><?php echo $fishlist['price'] ?></td>
                         </tr>                                                                                                                                                                                 
                                                                                                                                                                                                               
@@ -139,8 +112,6 @@
                             </td>                                                                                                                                                                             
                             <td><img src="img/fish/lava/<?php echo $fishlist['id'] ?>.png" alt="fish"></td>                                                                                                   
                             <td><?php echo $fishlist['name_fr'] ?> / <?php echo $fishlist['name_eng'] ?></td>
-                            <td><?php echo $fishlist['liquid'] ?></td>
-                            <td><?php echo $fishlist['rarity'] ?></td>
                             <td><?php echo $fishlist['price'] ?></td>
                         </tr>                                                                                                                                                                                 
                                                                                                                                                                                                               
@@ -167,8 +138,6 @@
                             </td>                                                                                                                                                                             
                             <td><img src="img/fish/plasma/<?php echo $fishlist['id'] ?>.png" alt="fish"></td>                                                                                                 
                             <td><?php echo $fishlist['name_fr'] ?> / <?php echo $fishlist['name_eng'] ?></td>
-                            <td><?php echo $fishlist['liquid'] ?></td>
-                            <td><?php echo $fishlist['rarity'] ?></td>
                             <td><?php echo $fishlist['price'] ?></td>
                         </tr>                                                                                                                                                                                 
                                                                                                                                                                                                               
@@ -181,4 +150,32 @@
                 <input type="submit" name="submit" id="submit_fishs" value="validate the purchase">
             </div>                                                                                                                                                                                            
 
-    </form>
+            </form>                                                                                                                                                                                                   
+                                                                                                                                                                                                              
+            <!-- Init all display -->                                                                                                                                                                         
+    <script>                                                                                                                                                                                                  
+        var elems = document.getElementsByClassName('waterfishs');                                                                                                                                            
+        for (var i=0;i<elems.length;i+=1){                                                                                                                                                                    
+            elems[i].style.display = 'none';                                                                                                                                                                  
+        }                                                                                                                                                                                                     
+
+        var elems = document.getElementsByClassName('chocofishs');                                                                                                                                            
+        for (var i=0;i<elems.length;i+=1){                                                                                                                                                                    
+            elems[i].style.display = 'none';
+        }                                                                                                                                                                                                     
+
+        var elems = document.getElementsByClassName('lavafishs');
+        for (var i=0;i<elems.length;i+=1){
+            elems[i].style.display = 'none';
+        }                                                                                                                                                                                                     
+                                                                                                                                                                                                            
+        var elems = document.getElementsByClassName('plasmafishs');
+        for (var i=0;i<elems.length;i+=1){
+            elems[i].style.display = 'none';
+        }                                                                                                                                                                                                     
+    </script>                                                                                                                                                                                                 
+                                                                                                                                                                                                            
+    <script src="script/marketplace.js"></script>                                                                                                                                                                                                   
+    
+</body>                                                                                                                                                                                                       
+</html>      
